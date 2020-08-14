d03ea025d0ce1d4d2d3b49deea55d93dd0e64fe0
naresh0101 <meghnathakur484@gmail.com>
2020-08-14 11:26:52 +0530

    Updates

---
+++ precache-manifest.662623aabb7acb8d1a7b651cbb258f86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02fe5c7a7dc66ba2316787e572ac5a49",
+    "url": "/meghathakur23/winUall/index.html"
+  },
+  {
+    "revision": "17feb6f8b17a1ecc3606",
+    "url": "/meghathakur23/winUall/static/css/2.58a7a907.chunk.css"
+  },
+  {
+    "revision": "1bfde0d560d30dbd0470",
+    "url": "/meghathakur23/winUall/static/css/main.0e056e28.chunk.css"
+  },
+  {
+    "revision": "17feb6f8b17a1ecc3606",
+    "url": "/meghathakur23/winUall/static/js/2.606decd3.chunk.js"
+  },
+  {
+    "revision": "799f3d5c27694f0f3c92bbc8286bf099",
+    "url": "/meghathakur23/winUall/static/js/2.606decd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bfde0d560d30dbd0470",
+    "url": "/meghathakur23/winUall/static/js/main.415043cb.chunk.js"
+  },
+  {
+    "revision": "c716789860144ab04aeb",
+    "url": "/meghathakur23/winUall/static/js/runtime-main.b92da293.js"
+  }
+]);
